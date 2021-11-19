da73e6e067d9739167b61b2cfb0a5efe69d79fc1
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-11-19 19:27:21 +0530

    Rename App.js

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useMoralis } from "react-moralis";
+
+const App = () => {
+	console.log(process.env.REACT_APP_MORALIS_APPID);
+
+	const { authenticate, isAuthenticated, user } = useMoralis();
+
+	return !isAuthenticated ? (
+		<div>
+			<button onClick={() => authenticate()}>Authenticate</button>
+		</div>
+	) : (
+		<div>
+			<h1>Welcome {user.get("username")}</h1>
+			<div className="App">🚀 SIP Frontend </div>
+		</div>
+	);
+};
+
+export default App;
