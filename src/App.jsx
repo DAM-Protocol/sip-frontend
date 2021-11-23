32d3cdea2a5fe1d074884116ef62684fcf1073c4
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-11-23 15:57:43 +0530

    Page Loader and Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,16 @@ import { Routes, Route, Navigate } from "react-router";
 // Components
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-import NotFound from "./pages/NotFound";
+import PageLoader from "./components/PageLoader";
 
 // Styles
 import AppStyled from "./App.styles";
+import { lazy, Suspense } from "react";
+import { Link } from "react-router-dom";
+
+// Lazy Imports
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Components = lazy(() => import("./pages/Components"));
 
 const App = () => {
 	return (
@@ -18,7 +24,24 @@ const App = () => {
 					{/* Base Route */}
 					<Route path="/">
 						{/* Route `/` */}
-						<Route path="" exact element={<Home />} />
+						<Route
+							path=""
+							exact
+							element={
+								<Suspense fallback={<PageLoader />}>
+									<Home />
+								</Suspense>
+							}
+						/>
+						<Route
+							path="components"
+							exact
+							element={
+								<Suspense fallback={<PageLoader />}>
+									<Components />
+								</Suspense>
+							}
+						/>
 
 						<Route exact path="404" element={<NotFound />} />
 						{/* No Matching Route -> Redirect */}
---
+++ src/components/PageLoader.jsx
@@ -0,0 +1,10 @@
+import LoaderSVG from "../assets/Loader.svg";
+const PageLoader = () => {
+	return (
+		<div>
+			<img src={LoaderSVG} alt="loading" />
+		</div>
+	);
+};
+
+export default PageLoader;
