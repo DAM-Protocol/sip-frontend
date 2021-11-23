9bbf66a6b27e79cc1d3b9531adf7fd22aaf513c0
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-11-23 11:48:27 +0530

    Adobe Fonts

--- src/components/Button.styles.js
+++ src/components/Button.styles.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 const Button = styled.button`
 	font-size: 1.2rem;
-	padding: 0.35rem 1.25rem;
+	padding: 0.35rem 1.5rem;
 	background: none;
 
 	border-radius: 500px;
-	border: 3px solid ${(props) => props.theme.colors.primary.gold};
-	font-weight: 300;
+	border: 2px solid ${(props) => props.theme.colors.primary.gold};
+	font-weight: 400;
 	color: ${(props) => props.theme.colors.text};
 `;
 
