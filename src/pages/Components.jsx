f7de905277554657a6a975a8739cea3374434864
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2021-11-24 20:24:45 +0530

    Component Showcase

---
+++ src/pages/Components.jsx
@@ -0,0 +1,36 @@
+import Button from "../components/Button.styles";
+import Navbar from "../components/Navbar";
+import ComponentsStyled from "./Components.styles";
+
+const Components = () => {
+	return (
+		<ComponentsStyled>
+			<h1>All Components</h1>
+			<div className="organism level">
+				<h2>Organisms</h2>
+
+				<div className="component">
+					<h4>Navigation Bar</h4>
+					<Navbar />
+				</div>
+			</div>
+			<hr />
+
+			<div className="molecule level">
+				<h2>Molecules</h2>
+
+				<div className="component"></div>
+			</div>
+			<hr />
+			<div className="atom level">
+				<h3>Atoms</h3>
+				<Button stroke>Button</Button>
+				<Button filled>Button Filled</Button>
+				<Button alert>Button Alert</Button>
+			</div>
+			<hr />
+		</ComponentsStyled>
+	);
+};
+
+export default Components;
---
+++ src/pages/Components.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const ComponentsStyled = styled.div`
+	display: flex;
+	flex-direction: column;
+	.level {
+		margin: 1rem 0;
+		.component {
+			margin: 1rem 0;
+		}
+	}
+`;
+
+export default ComponentsStyled;
